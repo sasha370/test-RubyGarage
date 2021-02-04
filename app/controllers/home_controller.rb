364ce237ad447b19698076043ea49271787b2e6f
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-02-04 19:01:41 +0300

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-
     @projects = Project.where(user_id: current_user).order(created_at: :desc)
   end
-
-
 end
